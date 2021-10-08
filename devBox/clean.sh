6f09fa9f4414392a02685e33a01a763049497115
Catalin Pop <popcatalin118@gmail.com>
2021-10-08 17:37:33 -0400

    modified clean script

--- devBox/clean.sh
+++ devBox/clean.sh
@@ -2,19 +2,9 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-TERRAFORM_DIR=${DIR}/terraform
 
 # Verify terraform is installed and correct version
-source ${DIR}/../env_check/version_check.sh
 
-
-
-# check all required variables are set
-source ${DIR}/../env_check/variables_check.sh
-
-cp ${DIR}/terraform.tfvars $TERRAFORM_DIR
-
-# Starting terraform
-# ${TERRAFORM_DIR}/terraform init
-
-# ${TERRAFORM_DIR}/terraform apply -auto-approve
+cd terraform
+rm terraform.tfstate*
+rm -rf .terraform
